ea9865f438345ea6da0ad859ae4f49db483fd35c
Arvid Norlander <anmaster@kuonet.org>
2007-10-20 18:37:54 +0200

    Fix bug in modules/m_join.sh that caused it to never send a reason on part.

--- modules/m_join.sh
+++ modules/m_join.sh
@@ -47,12 +47,12 @@ module_join_on_PRIVMSG() {
 	if parameters="$(parse_query_is_command "$query" "part")"; then
 		if [[ "$parameters" =~ ^(#[^ ]+)(\ (.+))? ]]; then
 			local channel="${BASH_REMATCH[1]}"
-			local message="${BASH_REMATCH[3]}"
+			local reason="${BASH_REMATCH[3]}"
 			if access_check_capab "join" "$sender" "$channel"; then
 				if [[ -z "$reason" ]]; then
 					channels_part "$channel"
 				else
-					channels_part "$channel" "$message"
+					channels_part "$channel" "$reason"
 				fi
 			else
 				access_fail "$sender" "make the bot part channel" "join"
