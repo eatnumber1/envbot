04f4cc16d425f988f433086414b136c9c6da3ada
Arvid Norlander <anmaster@kuonet.org>
2007-11-13 13:32:23 +0100

    Some more I missed in last commit

--- tools/bashdoc/bashdoc.sh
+++ tools/bashdoc/bashdoc.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# -*- coding: UTF8 -*-
 #--------------------------
 ##	@Synopsis	Reads specialy formated shell scripts and creates docs
 ##	@Copyright	Copyright 2003, Paul Mahon
