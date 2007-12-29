fdfbc6b48fb88d3e66497428c2e42eb948499b55
Vsevolod Kozlov <sevakda@gmail.com>
2007-12-29 15:08:47 +0300

    Bunch of changes to m_dice.sh: grammar fixes, shortened code

--- modules/m_dice.sh
+++ modules/m_dice.sh
@@ -48,7 +48,7 @@ module_dice_handler_roll() {
 	local parameters="$3"
 	if [[ $parameters =~ ^([0-9]+)d([0-9]+)$ ]]; then
 		local how_much_times="${BASH_REMATCH[1]}"
-		local how_much_sides="${BASH_REMATCH[2]}"
+		local how_many_sides="${BASH_REMATCH[2]}"
 		local target=
 		if [[ $2 =~ ^# ]]; then
 			target="$2"
@@ -57,25 +57,24 @@ module_dice_handler_roll() {
 		fi
 		local insane=0
 		# Chech if number of dies and sides are sane.
-		if (( ($how_much_sides < 2 || $how_much_sides > 100)
+		if (( ($how_many_sides < 2 || $how_many_sides > 100)
 			 || ($how_much_times < 1 || $how_much_times > 100) )); then
-			 log_warning "Tried to roll $how_much_times dies $how_much_sides each!"
+			 log_warning "Tried to roll $how_much_times dies $how_many_sides sides each!"
 			 log_warning "This is above the allowed maximum or below the allowed minimum, and was aborted."
 			 send_msg "$target" "You can't roll that."
 			 return 0
 		fi
-		# Roll $how_much_times dies, each with $how_much_sides sides.
-		local result=
+		# Roll $how_much_times dies, each with $how_many_sides sides.
+		local result=""
 		local total=0
 		for (( i=0; $i < $how_much_times; i+=1 )); do
-			local rolled=$(( ($RANDOM % $how_much_sides) + 1 ))
+			local rolled=$(( ($RANDOM % $how_many_sides) + 1 ))
 			result+="$rolled, "
 			((total += $rolled))
 		done
+		result=${result%, }
 		if [[ $how_much_times != 1 ]]; then
-			result+="with the grand total of $total"
-		else
-			result="$total" # Yes, this could still probably be done better, since here we remove all the result we were generating so long..
+			result+=" with the grand total of $total"
 		fi
 		send_msg "$target" "You rolled ${result}."
 	else
