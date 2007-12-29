9485152f61491e626eb7623d0e9bd6e18f07a630
Vsevolod Kozlov <sevakda@gmail.com>
2007-12-29 13:20:21 +0300

    Added help entries to m_dice.sh

--- modules/m_dice.sh
+++ modules/m_dice.sh
@@ -1,14 +1,42 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
+###########################################################################
+#                                                                         #
+#  envbot - an IRC bot in bash                                            #
+#  Copyright (C) 2007  Arvid Norlander                                    #
+#  Copyright (C) 2007  Vsevolod Kozlov                                    #
+#                                                                         #
+#  This program is free software: you can redistribute it and/or modify   #
+#  it under the terms of the GNU General Public License as published by   #
+#  the Free Software Foundation, either version 3 of the License, or      #
+#  (at your option) any later version.                                    #
+#                                                                         #
+#  This program is distributed in the hope that it will be useful,        #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of         #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #
+#  GNU General Public License for more details.                           #
+#                                                                         #
+#  You should have received a copy of the GNU General Public License      #
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.  #
+#                                                                         #
+###########################################################################
+#---------------------------------------------------------------------
+## Rolls dies.
+#---------------------------------------------------------------------
 
 module_dice_INIT() {
 	modinit_API='2'
 	modinit_HOOKS=''
 	commands_register "$1" 'roll' || return 1
+	helpentry_module_dice_description="Rolls dies for you."
+
+	helpentry_dice_roll_syntax="<dies>d<sides>"
+	helpentry_dice_roll_description="Rolls <dies> dies, each <sides> sides."
 }
 
 module_dice_UNLOAD() {
-	return 0
+	unset helpentry_module_dice_description
+	unset helpentry_dice_roll_syntax helpentry_dice_roll_description
 }
 
 module_dice_REHASH() {
